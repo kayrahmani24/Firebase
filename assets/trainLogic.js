66bf2e3fca37d91700d0d5bce078ea40be809069
Kayvan Rahmani <kayvanrahmani@Kayvans-MacBook-Air.local>
2017-10-23 13:03:53 -0500

    added moment js

--- assets/trainLogic.js
+++ assets/trainLogic.js
@@ -1,3 +1,6 @@
+
+  
+
  console.log("hey");
 
  var config = {
@@ -15,13 +18,13 @@
   	event.preventDefault();
 
 var trainLine = $("#train-line").val().trim();
-var destination = $("#destination").val().trim();
+var trainDestination = $("#destination").val().trim();
 var firstTrain = $("#first-train").val().trim();
 var trainFrequency = $("#frequency-train").val().trim();
 
 var newTrain = {
 	train : trainLine,
-	destination : destination,
+	destination : trainDestination,
 	firstTrain : firstTrain,
 	frequency : trainFrequency
 };
@@ -41,4 +44,54 @@ $("#first-train").val("");
 $("#frequency-train").val("");
 
   });
+
+  
+
+
+ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+  	var trainLine = childSnapshot.val().train
+  	var trainDestination = childSnapshot.val().destination
+  	var firstTrain = childSnapshot.val().firstTrain
+  	var trainFrequency = childSnapshot.val().frequency
+
+
+  var tFrequency = 5;
+
+    
+    var firstTime = "4:00";
+
+    var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+  
+    var currentTime = moment();
+   
+
+   
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    
+    var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+
+   
+    var minutesAway = tFrequency - tRemainder;
+
+    if (minutesAway == '5'|| "0") {
+      minutesAway == "Now";
+    } else {
+      minutesAway == tFrequency - tRemainder;
+    };
+    
+
+    
+    var nextTime = moment().add(minutesAway, "minutes");
+   nextArrival = moment(nextTime).format("hh:mm");
+
+$("#train-table > tbody").append("<tr><td>" + trainLine + "</td><td>" + trainDestination + "</td><td>" +
+  trainFrequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
+});
+
 
